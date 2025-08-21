8eb5bee17daa6d6d41b32df00cfbb4a42a1f1f07
rachit-walia <rachitwalia3@gmail.com>
2025-08-21 12:57:55 +0530

    routes update

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,22 +1,64 @@
 const express = require("express");
 const passport = require("passport");
 const {
-  getAllPosts,
+  getPosts,        // handles search/author filters OR returns all
   createPost,
   updatePost,
   deletePost,
   addComment,
-    getPosts,
 } = require("../controllers/postControllers");
 
 const router = express.Router();
 
-router.get("/", getAllPosts);
+// Public routes
+router.get("/", getPosts);  // GET http://localhost:4500/api/posts?search=xyz&author=123
 
-router.get("/", getPosts);
+// Protected routes
 router.post("/", passport.authenticate("jwt", { session: false }), createPost);
 router.put("/:id", passport.authenticate("jwt", { session: false }), updatePost);
 router.delete("/:id", passport.authenticate("jwt", { session: false }), deletePost);
 router.post("/:id/comment", passport.authenticate("jwt", { session: false }), addComment);
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const passport = require("passport");
+// const {
+//   getAllPosts,
+//   createPost,
+//   updatePost,
+//   deletePost,
+//   addComment,
+//   getPosts,
+// } = require("../controllers/postControllers");
+
+// const router = express.Router();
+
+
+// router.get("/all", getAllPosts);   
+// // GET http://localhost:4500/api/getAllPosts/all
+// router.get("/", getPosts);         
+// // GET http://localhost:4500/api/getAllPosts 
+
+
+// router.post("/", passport.authenticate("jwt", { session: false }), createPost);
+// router.put("/:id", passport.authenticate("jwt", { session: false }), updatePost);
+// router.delete("/:id", passport.authenticate("jwt", { session: false }), deletePost);
+// router.post("/:id/comment", passport.authenticate("jwt", { session: false }), addComment);
+
+// module.exports = router;
